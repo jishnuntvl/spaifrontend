1974c44215ca296b73ad0b82fc96aeb2da01c17b
jishnu <jishnuntvl747@gmail.com>
2024-07-13 14:57:50 +0530

    animation

--- spai.js
+++ spai.js
@@ -13,4 +13,85 @@ setInterval(() => {
 heroPics[currentPic].classList.add('active');
 heroTexts[currentPic].classList.add('active');
 
+/*****news*************************** */
+document.addEventListener('DOMContentLoaded', function () {
+    const floatingDivs = document.querySelectorAll('.news-containers');
 
+    const observerOptions = {
+        root: null, // Use the viewport as the container
+        rootMargin: '0px',
+        threshold: 0.5 // Trigger when 10% of the element is visible
+    };
+
+    function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(handleIntersection, observerOptions);
+
+    floatingDivs.forEach(div => {
+        observer.observe(div);
+    });
+});
+
+/************************************************/
+
+/**********associa*/
+document.addEventListener('DOMContentLoaded', function () {
+    const floatingDivs = document.querySelectorAll('.associate-containers');
+
+    const observerOptions = {
+        root: null, // Use the viewport as the container
+        rootMargin: '0px',
+        threshold: 0.8 // Trigger when 10% of the element is visible
+    };
+
+    function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(handleIntersection, observerOptions);
+
+    floatingDivs.forEach(div => {
+        observer.observe(div);
+    });
+});
+
+/****************about********/
+document.addEventListener('DOMContentLoaded', function () {
+    const floatingDivs = document.querySelectorAll('.about-content');
+
+    const observerOptions = {
+        root: null, // Use the viewport as the container
+        rootMargin: '0px',
+        threshold: 0.5 // Trigger when 10% of the element is visible
+    };
+
+    function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(handleIntersection, observerOptions);
+
+    floatingDivs.forEach(div => {
+        observer.observe(div);
+    });
+});
